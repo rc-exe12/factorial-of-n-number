f49799741a5911eab428efcf0372726d978c94f1
Ramcharan Sai <25r11a05ff@gcet.edu.in>
2025-11-28 14:11:57 +0530

    Refactor factorial calculation into separate function

--- factorial.c
+++ factorial.c
@@ -1,14 +1,19 @@
 #include<stdio.h>
-int main(){
-    printf("Enter the value of n( for factorial)    : ");
-    int n;
-    scanf("%d",&n);
+int fac_n(int n){
     int f=1;
     while(n>1){
         f=f*n;
         n--;
-            }
-    printf("Factorial is : %d",f);
+    }
+        return f;
+    }
+int main(){
+    printf("Enter the value of n( for factorial)    : ");
+    int n;
+    scanf("%d",&n);
+    
+        int x=fac_n(n);
+    printf("Factorial is : %d",x);
+   
     return 0;
-
 }
