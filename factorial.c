4d16c86d01ea72afc3b932c433d370765d494b01
Ramcharan Sai <25r11a05ff@gcet.edu.in>
2025-11-29 12:46:33 +0530

    Refactor factorial function to use recursion

--- factorial.c
+++ factorial.c
@@ -1,19 +1,23 @@
 #include<stdio.h>
-int fac_n(int n){
-    int f=1;
-    while(n>1){
-        f=f*n;
-        n--;
-    }
-        return f;
-    }
+int fac_n(int n);
 int main(){
     printf("Enter the value of n( for factorial)    : ");
     int n;
     scanf("%d",&n);
-    
-        int x=fac_n(n);
+      int x=fac_n(n);
     printf("Factorial is : %d",x);
    
     return 0;
 }
+int fac_n(int n){
+    if(n==1){
+     return 1;
+    }
+else
+ {
+    int f=1;
+    f=n*fac_n(n-1);
+    return f;
+}
+}
+
